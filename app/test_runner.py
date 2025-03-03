3872113246d440183ae8f8ac62aef4bf7703a64a
ayatsuliak <andrii.yatsulaik@gmail.com>
2025-03-03 17:30:54 +0200

    Initial commit

--- app/test_runner.py
+++ app/test_runner.py
@@ -5,9 +5,8 @@ from app.config import APPIUM_CAPS, APPIUM_SERVER
 
 def run_test():
     driver = webdriver.Remote(APPIUM_SERVER, APPIUM_CAPS)
-    time.sleep(5)  # Чекаємо запуск додатка
+    time.sleep(5)
 
-    # Тут буде логіка тесту (пошук готелю, отримання цін тощо)
 
     driver.quit()
 
